2da93195a178a64e53e42d02ebbeb3ba656b15ae
Brittany Kerr <kerrbrittany9@gmail.com>
2017-09-21 15:39:49 -0700

    make template

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(props) {
+  return (
+    <div>
+      <p>Hi</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from  "./components/App";
+import { AppContainer } from 'react-hot-loader';
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('react-app-root')
+);
