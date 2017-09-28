f1605234c61e1590a5ed833726bef439c0fb8899
Brittany Kerr and Calla Rudolph <calla@>
2017-09-28 08:36:40 -0700

    add dependencies to connect to store/use api

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from  "./components/App";
 import { AppContainer } from 'react-hot-loader';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('react-app-root')
+ <Provider store={store}>
+   <App />
+ </Provider>,
+ document.getElementById('react-app-root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'template',
+      title: 'dog-call',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
