2d1fb176f66ec3c9c29baf958152f6d8d1c1d260
Brittany Kerr and Calla Rudolph <calla@>
2017-09-28 10:59:20 -0700

    add conditional to formAreaContent for UI

--- src/components/DogDisplay.jsx
+++ src/components/DogDisplay.jsx
@@ -4,12 +4,25 @@ import { connect } from 'react-redux';
 
 
 const DogDisplay = ({ dispatch, dog })=> {
+  console.log(dog);
+  let formAreaContent;
+  if (dog.dogId === 0) {
+    formAreaContent =
+      <div>
+        <p>A dog will be here soon.</p>
+      </div>
+  } else {
+    formAreaContent =
+      <div>
+        <p>Your results for {dog.dogSearch}</p>
+        <img src={dog.dog}/>
+      </div>
+  }
   return (
     <div>
       <DogSearch/>
       <br/>
-      <p>Your results for {dog.dogSearch}</p>
-      <img src={dog.dog}/>
+      {formAreaContent}
     </div>
   );
 }
